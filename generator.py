92718f8d4d9efcff35d94a24ad155bb2488a343f
Michal Janczykowski <mjanczykowski@gmail.com>
2017-06-08 00:37:49 +0200

    test data set

--- generator.py
+++ generator.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 import numpy as np
+import random
 
 
 def rolling_window(a, window):
@@ -10,13 +11,31 @@ def rolling_window(a, window):
     return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)
 
 
+def distort(distort_level):
+    return 1 + random.uniform(-distort_level, distort_level)
+
+
 if __name__ == '__main__':
     window_size = 32
-    days = 700
+    days = 365
     hours = days * 24
     half_hours = hours * 2
-    observations = np.array([1000 * np.sin(np.pi / 24.0 * x * 0.5) ** 2 for x in xrange(half_hours)], dtype=int)
+    distortion = 0.1
+    observations = np.array(
+        [distort(distortion) * 1000 * np.sin(np.pi / 24.0 * x * 0.5) ** 2 for x in range(half_hours)], dtype=int)
     X = rolling_window(observations, window_size)[:-1, :]
     y = np.array(observations[window_size:])
-    np.save('sin2X', X)
-    np.save('sin2y', y)
+    np.save('sin2X_train', X)
+    np.save('sin2y_train', y)
+
+    test_days = 7
+    half_h = test_days * 24 * 2
+    anomaly_observation = np.array(
+        [distort(distortion) * 1000 * np.sin(np.pi / 24.0 * x * 0.5) ** 2 for x in range(int(half_h * 0.45))] +
+        [distort(distortion) * 0 * np.sin(np.pi / 24.0 * x * 0.5) ** 2 for x in range(int(half_h * 0.45), int(half_h * 0.7))] +
+        [distort(distortion) * 1000 * np.sin(np.pi / 24.0 * x * 0.5) ** 2 for x in range(int(half_h * 0.7), half_h)],
+        dtype=int)
+    X_anomaly = rolling_window(anomaly_observation, window_size)[:-1, :]
+    y_anomaly = np.array(anomaly_observation[window_size:])
+    np.save('sin2X_test', X_anomaly)
+    np.save('sin2y_test', y_anomaly)
--- lstm.py
+++ lstm.py
@@ -9,34 +9,46 @@ from matplotlib import pyplot as plt
 
 window_size = 32
 
-X = np.load('sin2X.npy')
-y = np.load('sin2y.npy')
+X_train = np.load('sin2X_train.npy')
+y_train = np.load('sin2y_train.npy')
 
 Xscaler = MinMaxScaler(feature_range=(0, 1))
 yscaler = MinMaxScaler(feature_range=(0, 1))
-X = Xscaler.fit_transform(X)
-y = yscaler.fit_transform(y)
+X_train = Xscaler.fit_transform(X_train)
+y_train = yscaler.fit_transform(y_train)
 
-X = X.reshape(X.shape + (1,))
+X_train = X_train.reshape(X_train.shape + (1,))
 
-max_X = np.max(np.max(X))
+max_X = np.max(np.max(X_train))
 
 model = Sequential()
 model.add(LSTM(4, input_shape=(window_size, 1)))
 model.add(Dense(1))
 model.compile(loss='mean_squared_error', optimizer='adam')
 
-print model.summary()
+print(model.summary())
 
-model.fit(X, y, epochs=2)
+model.fit(X_train, y_train, epochs=2)
 
-y_real = yscaler.inverse_transform(y)
-predicted = model.predict(X)
+# TEST
+
+X_test = np.load('sin2X_test.npy')
+y_test = np.load('sin2y_test.npy')
+
+Xscaler = MinMaxScaler(feature_range=(0, 1))
+yscaler = MinMaxScaler(feature_range=(0, 1))
+X_test = Xscaler.fit_transform(X_test)
+y_test = yscaler.fit_transform(y_test)
+
+X_test = X_test.reshape(X_test.shape + (1,))
+
+y_real = yscaler.inverse_transform(y_test)
+predicted = model.predict(X_test)
 predicted_real = np.reshape(yscaler.inverse_transform(predicted), (len(y_real),))
-print predicted_real, y_real
+print(predicted_real, y_real)
 
-xs = np.array([0.5 * x for x in xrange(len(y_real))])
+xs = np.array([0.5 * x for x in range(len(y_real))])
 
-plt.plot(xs[:100], y_real[:100], 'g-')
-plt.plot(xs[:100], predicted_real[:100], 'r-')
+plt.plot(xs[:500], y_real[:500], 'g-')
+plt.plot(xs[:500], predicted_real[:500], 'r-')
 plt.show()
